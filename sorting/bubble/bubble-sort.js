32532d9d2b2082ff7868358b46091b64ca1ed259
IRY21 <rilok21work@gmail.com>
2018-10-15 13:36:58 +0500

    start

---
+++ sorting/bubble/bubble-sort.js
@@ -0,0 +1,23 @@
+function bubbleSort(arr) {
+    let lastIndex = arr.length - 1;
+    let sorted = false;
+
+    while(!sorted) {
+        sorted = true;
+
+        for(let i = 0; i < lastIndex; i++) {
+            let current = arr[i];
+            let next = arr[i + 1];
+
+            if(current > next) {
+                sorted = false;
+                arr[i] = next;
+                arr[i + 1] = current;
+            }
+        }
+
+        lastIndex--;
+    }
+}
+
+module.export = bubbleSort;
